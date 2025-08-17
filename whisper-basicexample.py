4064ccf43a7e659ef848f782272c4cdc9adc62ae
Emily Wilbourn <74464395+ewilbourn@users.noreply.github.com>
2025-08-17 18:37:44 -0500

    Add comment with link to audio file to basic example

--- whisper-basicexample.py
+++ whisper-basicexample.py
@@ -1,7 +1,9 @@
 import whisper
 
 model = whisper.load_model("base")
+# m4a file can be downloaded here: https://aka.ms/mslearn-speech-files 
 audio = "C:/Users/ewilb/Downloads/Speech/WhatAICanDo.m4a"
 
 result = model.transcribe(audio, fp16=False, language="en")
+
 print(result["text"])
